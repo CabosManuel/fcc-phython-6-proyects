89cc7a94248c3f11961576d30f114228be0d8a8a
CabosManuel <cabos.apolaya@gmail.com>
2023-10-13 22:12:49 -0500

    Mad libs (Historias locas)

---
+++ 01_mad_libs.py
@@ -0,0 +1,18 @@
+# Concatenate strings:
+# Learn to program with ______________.
+
+company = "freeCodeCamp"
+
+# print("Learn to program with " + company)
+# print("Learn to program with {}".format(company))
+# print(f"Learn to program with {company}") # f-string
+
+# Mad Libs
+
+adj1 = input("Adjective 1: ")
+adj2 = input("Adjective 1: ")
+subst = input("Substantive: ")
+
+madLibs = f"¡Programming is {adj1}! I understand much {adj2} when developing a {subst}"
+
+print(madLibs)
